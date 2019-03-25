da40b428b354a510a0e51abcb8a2a71ec0dcd280
Gordon Klaus <gordon.klaus@telenordigital.com>
2019-03-25 17:24:34 +0100

    Copy service configs in case they have changed.

--- scripts/cron_e2e.sh
+++ scripts/cron_e2e.sh
@@ -12,11 +12,14 @@ if [ -e ~/.arduino-config.json ]; then
     /usr/local/go/bin/go build | ts
     git checkout -- ../go.sum # discard local modifications to go.sum
     echo restart arduino service | ts
-    sudo systemctl stop arduino | ts
-    sudo systemctl start arduino | ts
+	sudo cp ~/Arduino/nbiot-e2e/arduino-service/arduino.service /etc/systemd/system/
+    sudo systemctl daemon-reload | ts
+    sudo systemctl restart arduino | ts
 fi
 
 # restart nbiot-service
 echo restart nbiot service | ts
+sudo cp ~/Arduino/nbiot-e2e/nbiot-service/nbiot.service /etc/systemd/system/
+sudo systemctl daemon-reload | ts
 sudo systemctl restart nbiot | ts
 ) &>> ~/log/nbiot-e2e.log
