0867d15db44506cf1ddf4575ab67a00987fc24de
Gordon Klaus <gordon.klaus@telenordigital.com>
2019-05-06 12:28:47 +0200

    Remove dead code.

--- nbiot-service/serial/ser.go
+++ nbiot-service/serial/ser.go
@@ -2,7 +2,6 @@ package serial
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"log"
 	"strings"
@@ -28,7 +27,6 @@ func NewSerialConnection(device string, baud int, verbose bool) (*SerialConnecti
 
 	// Wrap serial connection in scanner
 	scanner := bufio.NewScanner(s)
-	// scanner.Split(scanCRLF)
 
 	return &SerialConnection{
 		serialPort: s,
@@ -101,27 +99,4 @@ func (s *SerialConnection) scanResponse() ([]string, []string, error) {
 	}
 
 	return s.splitURCResponse(data, fmt.Errorf("Invalid response: '%v'", data))
-}
-
-func dropCR(data []byte) []byte {
-	if len(data) > 0 && data[len(data)-1] == '\r' {
-		return data[0 : len(data)-1]
-	}
-	return data
-}
-
-func scanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-	if i := bytes.Index(data, []byte{'\r', '\n'}); i >= 0 {
-		// We have a full newline-terminated line.
-		return i + 2, dropCR(data[0:i]), nil
-	}
-	// If we're at EOF, we have a final, non-terminated line. Return it.
-	if atEOF {
-		return len(data), dropCR(data), nil
-	}
-	// Request more data.
-	return 0, nil, nil
 }
